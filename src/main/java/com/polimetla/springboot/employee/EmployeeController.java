4dbd53e7e4f239c4a1273ba109368b034e0de522
pallabacharyya <49872035+pallabacharyya@users.noreply.github.com>
2020-07-16 01:47:25 +0530

    Update EmployeeController.java

--- src/main/java/com/polimetla/springboot/employee/EmployeeController.java
+++ src/main/java/com/polimetla/springboot/employee/EmployeeController.java
@@ -11,27 +11,27 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @RequestMapping("/employees")
+    @RequestMapping("/employee")
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
 
-    @RequestMapping("/employees/{employeeId}")
+    @RequestMapping("/employee/{employeeId}")
     public Employee getEmployeeById(@PathVariable int employeeId) {
         return employeeService.getEmployeeById(employeeId);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/employees")
+    @RequestMapping(method = RequestMethod.POST, value = "/employee")
     public void createEmployee(@RequestBody Employee employee) {
         employeeService.createEmployee(employee);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/employees/{employeeId}")
+    @RequestMapping(method = RequestMethod.PUT, value = "/employee/{employeeId}")
     public void updateEmployee(@RequestBody Employee employee, @PathVariable int employeeId) {
         employeeService.updateEmployee(employee, employeeId);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/employees/{employeeId}")
+    @RequestMapping(method = RequestMethod.DELETE, value = "/employee/{employeeId}")
     public void deleteEmployee(@PathVariable int employeeId) {
         employeeService.deleteEmployee(employeeId);
     }
