d72b3e37f601a7190b80a485e2e562f30f7f5e56
pallabacharyya <49872035+pallabacharyya@users.noreply.github.com>
2020-07-15 22:01:38 +0530

    Add files via upload

---
+++ src/main/resources/Data.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS Employee;
+
+CREATE TABLE Employee (
+  employeeId INT AUTO_INCREMENT  PRIMARY KEY,
+  firstName VARCHAR(250) NOT NULL,
+  lastName VARCHAR(250) NOT NULL,
+  age INT
+);
